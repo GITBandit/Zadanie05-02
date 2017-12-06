1f66b07a30da1f04142c8a9a805e26b61fe50691
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-12-06 19:29:01 +0100

    Tablel created and sum printed.

---
+++ src/Table.java
@@ -0,0 +1,10 @@
+public class Table {
+
+    public static void main(String[] args) {
+
+        int[] tablica = {1, 2, 3, 4, 5};
+
+        System.out.println(tablica[0] + tablica[2] + tablica[4]);
+    }
+
+}
